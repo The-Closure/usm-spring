d0bb7cb96b22a1285001f5b23ca3e1259006d347
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-03-18 17:48:47 +0200

    --init board Module

---
+++ app/src/main/java/org/closure/app/boardModule/dto/BoarderResponse.java
@@ -0,0 +1,17 @@
+package org.closure.app.boardModule.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+public class BoarderResponse {
+    private Long id;
+    private String name;
+    private String image;
+    private String description;
+}
---
+++ app/src/main/java/org/closure/app/boardModule/exceptions/BorderErrorException.java
@@ -0,0 +1,13 @@
+package org.closure.app.boardModule.exceptions;
+
+public class BorderErrorException extends RuntimeException{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public BorderErrorException(String message)
+    {
+        super(message);
+    }
+}
