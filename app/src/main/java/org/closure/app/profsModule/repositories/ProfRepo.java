df017c288d534a7e6685543f6c8bfe11ebf31ce8
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-03-18 21:15:50 +0200

    -- init profs module

--- app/src/main/java/org/closure/app/profsModule/repositories/ProfRepo.java
+++ app/src/main/java/org/closure/app/profsModule/repositories/ProfRepo.java
@@ -1,9 +1,10 @@
 package org.closure.app.profsModule.repositories;
 
+import org.closure.app.entities.ProfsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfRepo extends JpaRepository {
+public interface ProfRepo extends JpaRepository<ProfsEntity, Long> {
     
 }
