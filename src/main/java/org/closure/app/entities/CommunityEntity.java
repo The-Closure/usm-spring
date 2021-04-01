a01f225a9c8df06ffce8845e110f0e7c10af71e5
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-04-01 23:20:06 +0300

    -- final fix before ultra features

--- src/main/java/org/closure/app/entities/CommunityEntity.java
+++ src/main/java/org/closure/app/entities/CommunityEntity.java
@@ -14,13 +14,7 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import org.closure.app.CommunityModule.dto.CommunityResponse;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 @Entity
-@Getter
-@Setter
-@Builder
 @Table(name = "communities")
 @JsonIdentityInfo(
     generator = ObjectIdGenerators.PropertyGenerator.class,
@@ -49,5 +43,123 @@ public class CommunityEntity {
             .name(name);
     }
 
+
+    public CommunityEntity() {
+    }
+
+    public CommunityEntity(Long id, String name, String description, String img, List<UserEntity> users, List<PostEntity> posts) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.img = img;
+        this.users = users;
+        this.posts = posts;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return this.img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public List<UserEntity> getUsers() {
+        return this.users;
+    }
+
+    public void setUsers(List<UserEntity> users) {
+        this.users = users;
+    }
+
+    public List<PostEntity> getPosts() {
+        return this.posts;
+    }
+
+    public void setPosts(List<PostEntity> posts) {
+        this.posts = posts;
+    }
+
+    public CommunityEntity id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public CommunityEntity name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public CommunityEntity description(String description) {
+        setDescription(description);
+        return this;
+    }
+
+    public CommunityEntity img(String img) {
+        setImg(img);
+        return this;
+    }
+
+    public CommunityEntity users(List<UserEntity> users) {
+        setUsers(users);
+        return this;
+    }
+
+    public CommunityEntity posts(List<PostEntity> posts) {
+        setPosts(posts);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof CommunityEntity)) {
+            return false;
+        }
+        CommunityEntity communityEntity = (CommunityEntity) o;
+        return Objects.equals(id, communityEntity.id) && Objects.equals(name, communityEntity.name) && Objects.equals(description, communityEntity.description) && Objects.equals(img, communityEntity.img) && Objects.equals(users, communityEntity.users) && Objects.equals(posts, communityEntity.posts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, img, users, posts);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", img='" + getImg() + "'" +
+            ", users='" + getUsers() + "'" +
+            ", posts='" + getPosts() + "'" +
+            "}";
+    }
   
 }
